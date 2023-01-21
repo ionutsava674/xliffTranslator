ac190d03d38b33ef8c6f152b0446b2b53f03c8a0
Ionut Sava <ionut.sava.8@gmail.com>
2023-01-21 22:54:02 +0200

    Initial Commit

---
+++ xliffTranslator/xliffTranslatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  xliffTranslatorApp.swift
+//  xliffTranslator
+//
+//  Created by Ionut Sava on 21.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct xliffTranslatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
